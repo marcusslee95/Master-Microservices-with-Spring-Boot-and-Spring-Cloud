7ff872fbaf4079fa370e1af76a756f542d3dfb72
Marcus Lee <marcusslee95@berkeley.edu>
2021-04-22 08:53:10 -0700

    recreated the limits-service project cuz first time around had named it improperly..... and initialized spring cloud config server

---
+++ section6-just-microservices/limits-service/src/test/java/section6/just/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package section6.just.microservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ section6-just-microservices/spring-cloud-config-server/src/test/java/section6/just/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package section6.just.microservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
