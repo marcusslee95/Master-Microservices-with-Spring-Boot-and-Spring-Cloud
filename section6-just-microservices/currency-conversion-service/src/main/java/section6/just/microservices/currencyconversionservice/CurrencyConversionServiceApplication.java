143c5c15e5a175470bcfe97d3c68842cca2c3896
Marcus Lee <marcusslee95@berkeley.edu>
2021-04-23 23:44:23 -0700

    created a new service that will use currency exchange service to do the currency conversion

---
+++ section6-just-microservices/currency-conversion-service/src/main/java/section6/just/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,13 @@
+package section6.just.microservices.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+}
---
+++ section6-just-microservices/currency-conversion-service/src/test/java/section6/just/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package section6.just.microservices.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
