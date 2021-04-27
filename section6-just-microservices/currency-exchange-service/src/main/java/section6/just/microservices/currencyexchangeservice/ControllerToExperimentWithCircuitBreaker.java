861999b681098554916ca3ea3391884cd043e5a6
Marcus Lee <marcusslee95@berkeley.edu>
2021-04-26 19:32:12 -0700

    adding retry to an endpoint which is just to say.... if I get an error as a response to a request.... I will send request a couple more times just to see if it will work... cuz maybe the service is just down momentarily... and then only after all the attempts fail will i actually send the error response back

--- section6-just-microservices/currency-exchange-service/src/main/java/section6/just/microservices/currencyexchangeservice/ControllerToExperimentWithCircuitBreaker.java
+++ section6-just-microservices/currency-exchange-service/src/main/java/section6/just/microservices/currencyexchangeservice/ControllerToExperimentWithCircuitBreaker.java
@@ -1,13 +1,29 @@
 package section6.just.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 public class ControllerToExperimentWithCircuitBreaker {
 	
+	private Logger logger =
+			LoggerFactory.getLogger(ControllerToExperimentWithCircuitBreaker.class); //can think of it as the thing that executes print statements... I'm not sure why we're using this instead of just System.out.println
+	
 	@GetMapping("/sample-endpoint")
+//	@Retry(name = "default") //just saying... hey if a request to this endpoint fails then try it again a couple times to see if it works.... before sending back an error
+	@Retry(name = "sample-api")
 	public String sampleEndpoint() {
-		return "Sample Endpoint";
+		logger.info("Request has been sent to Sample Endpoint");
+		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http:somenonexistenturl.com", 
+				String.class); //code to cause an error 
+		
+//		return "Sample Endpoint";
+		return forEntity.getBody();
 	}
 }
