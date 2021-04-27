011eda48732dbe5e05838c885878af499e98f3aa
Marcus Lee <marcusslee95@berkeley.edu>
2021-04-26 20:36:06 -0700

    used a circuitbreaker -> it's the solution for when service is down for a long time (not just momentarily down... in which case you'd use the retry in the line above)... where instead of keep on retrying to execute the request.... we retry to execute a request a couple times and if it's failing... determine something's wrong w/the service so we just stop sending the request and just send back some default response for awhile (cuz y send requests to something that we know isn't working)..... before sending a couple requests again just to see if the issue w/the service was resolved... and if the service is back up working properly... sending rqeuests again to it

--- section6-just-microservices/currency-exchange-service/src/main/java/section6/just/microservices/currencyexchangeservice/ControllerToExperimentWithCircuitBreaker.java
+++ section6-just-microservices/currency-exchange-service/src/main/java/section6/just/microservices/currencyexchangeservice/ControllerToExperimentWithCircuitBreaker.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -17,8 +18,9 @@ public class ControllerToExperimentWithCircuitBreaker {
 	
 	@GetMapping("/sample-endpoint")
 //	@Retry(name = "default") //just saying... hey if a request to this endpoint fails then try it again a couple times to see if it works.... before sending back an error
-//	@Retry(name = "sample-api")
-	@Retry(name = "sample-api", fallbackMethod="hardCodedResponse") //if all the retry attempts fail.... just send back return value of this method as response instead.... cuz otherwise we'd just be sending back an error
+//	@Retry(name = "sample-endpoint")
+//	@Retry(name = "sample-endpoint", fallbackMethod="hardCodedResponse") //if all the retry attempts fail.... just send back return value of this method as response instead.... cuz otherwise we'd just be sending back an error
+	@CircuitBreaker(name = "sample-endpoint", fallbackMethod="hardCodedResponse") // what is a circuitbreaker? It's the solution for when service is down for a long time (not just momentarily down... in which case you'd use the retry in the line above)... where instead of keep on retrying to execute the request.... we retry to execute a request a couple times and if it's failing... determine something's wrong w/the service so we just stop sending the request and just send back some default response for awhile (cuz y send requests to something that we know isn't working)..... before sending a couple requests again just to see if the issue w/the service was resolved... and if the service is back up working properly... sending rqeuests again to it 
 	public String sampleEndpoint() {
 		logger.info("Request has been sent to Sample Endpoint");
 		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http:somenonexistenturl.com", 
